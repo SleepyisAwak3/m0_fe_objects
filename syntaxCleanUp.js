8940bd82dd0e7f4afb30e0b7251b932a105011ac
Noah Byers <noahxbyers@gmail.com>
2022-11-10 16:41:53 -0800

    Add Syntax '

--- syntaxCleanUp.js
+++ syntaxCleanUp.js
@@ -1,15 +1,13 @@
-var foods = {apples: 23, 
-  grapes: 507, 
-  eggs: 48}
-
+var foods = {apples: 23, grapes: 507, eggs: 48};
 var person = { height: "6 ft", weight: "160 lbs" }
-
-var post = { imageSrc: "./images/beach.png",
-
-
-   caption: "At the beach with my besties", timestamp: "4:37 PM August 13, 2019",
-        number_likes: 0,
-comments: [] }
+var post = 
+{ 
+    imageSrc: "./images/beach.png", 
+    caption: "At the beach with my besties",
+    timestamp: "4:37 PM August 13, 2019",
+    number_likes: 0,
+    comments: [] 
+}
 
 
 var post2 =
